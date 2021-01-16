14b03d3950231568561941c781cbc32fd58c478b
sudomilk <mailertaylor@gmail.com>
2021-01-16 16:43:27 -0600

    nope, thats not how you add storage to a gke cluster

--- persistent-disk.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_disk" "default" {
-  name  = "test-disk"
-  type  = "pd-ssd"
-  zone  = "us-central1-a"
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
