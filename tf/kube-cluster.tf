0c91b533906f253cf07e596876575922d516bbf2
sudomilk <mailertaylor@gmail.com>
2021-01-15 19:54:27 -0600

    added providers.tf

--- tf/kube-cluster.tf
+++ tf/kube-cluster.tf
@@ -31,4 +31,3 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     ]
   }
 }
-
---
+++ tf/providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project     = "goldfishparty-cluster"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
