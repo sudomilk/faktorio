72a804a3a692c03befa8b42305def5f8c8b0e157
sudomilk <mailertaylor@gmail.com>
2021-01-17 15:14:42 -0600

    reworked auth for kube provider config

--- providers.tf
+++ providers.tf
@@ -6,5 +6,11 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
+  host = google_container_cluster.primary.endpoint
+
+  client_certificate     = file(google_container_cluster.primary.master_auth.0.client_certificate)
+  client_key             = file(google_container_cluster.primary.master_auth.0.client_key)
+  cluster_ca_certificate = file(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+
+  load_config_file = false # when you wish not to load the local config file
 }
