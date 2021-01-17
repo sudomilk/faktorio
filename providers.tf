17ebe846b8ded7a0c7e421db8397023c7d83274d
sudomilk <mailertaylor@gmail.com>
2021-01-17 15:34:33 -0600

    trying again with insecure = true

--- providers.tf
+++ providers.tf
@@ -12,5 +12,7 @@ provider "kubernetes" {
   client_key             = google_container_cluster.primary.master_auth.0.client_key
   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 
+  insecure = true
+
   load_config_file = false # when you wish not to load the local config file
 }
