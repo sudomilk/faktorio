1db329a0912a9a0044436de6218b3b94cac0a130
sudomilk <mailertaylor@gmail.com>
2021-01-17 15:16:09 -0600

    fixed file access lines in kube provi config

--- providers.tf
+++ providers.tf
@@ -8,9 +8,9 @@ provider "google" {
 provider "kubernetes" {
   host = google_container_cluster.primary.endpoint
 
-  client_certificate     = file(google_container_cluster.primary.master_auth.0.client_certificate)
-  client_key             = file(google_container_cluster.primary.master_auth.0.client_key)
-  cluster_ca_certificate = file(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 
   load_config_file = false # when you wish not to load the local config file
 }
