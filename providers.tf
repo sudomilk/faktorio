c37122af1c2755461a51e51ace1226c89839fb16
sudomilk <mailertaylor+github79@gmail.com>
2021-01-24 16:41:04 -0600

    going to learn first with kubectl

--- providers.tf
+++ providers.tf
@@ -4,9 +4,3 @@ provider "google" {
   zone        = "us-central1-c"
   credentials = var.gac
 }
-
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  token                  = var.gac
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-}
