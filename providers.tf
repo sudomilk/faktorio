9fe9a849e42071a10f6c25108fcbb09a3e4c993b
sudomilk <mailertaylor+github79@gmail.com>
2021-01-24 16:18:48 -0600

    removed datasource ref

--- providers.tf
+++ providers.tf
@@ -5,10 +5,8 @@ provider "google" {
   credentials = var.gac
 }
 
-data "google_client_config" "default" {}
-
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   token                  = var.gac
-  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
