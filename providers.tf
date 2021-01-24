391dc939edd1a1a7a9093f68b3637dd238cf7387
sudomilk <mailertaylor+github79@gmail.com>
2021-01-24 16:05:18 -0600

    seeing if this provider config works

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ provider "google" {
   credentials = var.gac
 }
 
-provider "kubernetes" {
-  host = google_container_cluster.primary.endpoint
+data "google_client_config" "default" {}
 
-  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-  client_key             = google_container_cluster.primary.master_auth.0.client_key
-  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
