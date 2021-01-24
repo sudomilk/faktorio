7da45c24f35c799289cd872a8b1fdd519995dc84
sudomilk <mailertaylor+github79@gmail.com>
2021-01-24 15:29:27 -0600

    testing without default config file param

--- providers.tf
+++ providers.tf
@@ -11,8 +11,4 @@ provider "kubernetes" {
   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
   client_key             = google_container_cluster.primary.master_auth.0.client_key
   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-
-  insecure = true
-
-  load_config_file = false # when you wish not to load the local config file
 }
